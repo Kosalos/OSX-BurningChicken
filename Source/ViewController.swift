497e5b9695322813783f29bb45ac2fcca03e465c
Harry Kosalos <kosalos@cox.net>
2019-04-24 23:06:34 -0700

    misspoke  earlier. It isn't Xcode 10.2 that freaks out but Swift 5. Note required change: The fractal shader is shared by both the 2D and 3D windows. The 2D window does not use buffer #0, and so I used to not initialize it. Swift 5 freaks out if you do that (viewDidAppear() is not even called!) Simple fix is to give it a buffer,,

--- Source/ViewController.swift
+++ Source/ViewController.swift
@@ -530,7 +530,7 @@ class ViewController: NSViewController, NSWindowDelegate, WGDelegate {
         commandEncoder.setTexture(texture1, index: 0)
         
         // skip unused buffer 0
-        // xcode 10.2   app is screwed up if you skip loading a buffer
+        // Swift 5   app is screwed up if you skip loading a buffer
         // so, load cBuffer, just to make it happy
         commandEncoder.setBuffer(cBuffer, offset: 0, index: 0)
         
